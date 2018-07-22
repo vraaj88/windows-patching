85ee9be54e6bf229bfa72017f0d0fe0154b793ab
krapa999 <36459275+krapa999@users.noreply.github.com>
2018-07-21 20:39:06 -0400

    Update windows-updateps.ps1

--- windows-updateps.ps1
+++ windows-updateps.ps1
@@ -1,6 +1,7 @@
 param (
     [string]$UserName = "Administrator",
-    [string]$Password = "yj*-!G=wSun2L=ru!5Qj8%.-3zcUArJ*"
+    [string]$Password = "yj*-!G=wSun2L=ru!5Qj8%.-3zcUArJ*",
+    [string]$Result = $('$Results')
 )
 
 # Set up auto-logon
@@ -64,12 +65,7 @@ Function InstallUpdates($Criteria, $Name) {
 }
 
 # First try Service Packs
-$Result = InstallUpdates "$BaseCriteria and CategoryIDs contains '68C5B0A3-D1A6-4553-AE49-01D3A7827828'" "Service Packs"
-$Result = InstallUpdates "$BaseCriteria and CategoryIDs contains '28BC880E-0592-4CBF-8F95-C79B17911D5F'" "Update Rollups"
-$Result = InstallUpdates "$BaseCriteria and CategoryIDs contains 'E6CF1350-C01B-414D-A61F-263D14D133B4'" "Critical Updates"
-$Result = InstallUpdates "$BaseCriteria and CategoryIDs contains '0FA1201D-4330-4FA8-8AE9-B877473B6441'" "Security Updates"
-$Result = InstallUpdates "$BaseCriteria and CategoryIDs contains 'E0789628-CE08-4437-BE74-2495B842F43B'" "Definition Updates"
-$Result = InstallUpdates "$BaseCriteria and CategoryIDs contains '5C9376AB-8CE6-464A-B136-22113DD69801'" "Application"
+
 
 Write-Host "All Updates Installed!"
 Write-Host ""
