cd1af75ad77d891f3e5ad9cfba53230e03a1cc18
krapa999 <36459275+krapa999@users.noreply.github.com>
2018-07-30 09:17:00 -0400

    Update windows-updateps.ps1

--- windows-updateps.ps1
+++ windows-updateps.ps1
@@ -1,6 +1,6 @@
 param (
-    [string]$UserName = $('$LocalAdm'),
-    [string]$Password = $('$LocalAdm')
+    [string]$UserName = "Administrator",
+    [string]$Password = "yj*-!G=wSun2L=ru!5Qj8%.-3zcUArJ*"
 )
 
 # Set up auto-logon
