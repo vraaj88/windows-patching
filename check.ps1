12a919d0bd7dfd2a0d9747a10feff7f79c949ae7
krapa999 <36459275+krapa999@users.noreply.github.com>
2018-08-16 18:31:08 +0530

    Create check.ps1

---
+++ check.ps1
@@ -0,0 +1,16 @@
+$arrService = Get-Service -Name AmazonCloudWatchAgent
+
+while ($arrService.Status -ne 'Running')
+{
+
+    Start-Service $ServiceName
+    write-host $arrService.status
+    write-host 'Service starting'
+    Start-Sleep -seconds 60
+    $arrService.Refresh()
+    if ($arrService.Status -eq 'Running')
+    {
+        Write-Host 'Service is now Running'
+    }
+
+}
